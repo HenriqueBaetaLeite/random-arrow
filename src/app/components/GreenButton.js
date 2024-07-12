90fb87e5f89794304a524355a3468ddefbeb6bb6
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2024-07-12 11:07:59 -0300

    refactor: change slider to appear numbers

--- src/app/components/GreenButton.js
+++ src/app/components/GreenButton.js
@@ -1,7 +1,7 @@
 const GreenButton = ({ text, onClicker }) => {
   return (
     <button
-      className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-3 transition"
+      className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-6 transition"
       onClick={() => onClicker()}
     >
       {text}
--- src/app/components/SlideSpeed.js
+++ src/app/components/SlideSpeed.js
@@ -1,14 +1,32 @@
-const SlideSpeed = ({ speed, setSpeed }) => (
-  <div className="m-4 flex flex-col justify-center items-center">
-    {/* <label className="m-1 text-black font-semibold">Velocidade:</label> */}
-    <input
-      type="range"
-      min={1000}
-      max={5000}
-      value={speed}
-      onChange={(e) => setSpeed(Number(e.target.value))}
-    />
-  </div>
-);
+const SlideSpeed = ({ speed, setSpeed }) => {
+  const marks = [1, 2, 3, 4, 5];
+
+  return (
+    <div className="m-4 flex flex-col justify-center items-center">
+      {/* <label htmlFor="speedRange" className="mb-2 text-black">
+        Velocidade do Slider: {speed / 1000} segundo(s)
+      </label> */}
+      <div className="relative w-full m-3">
+        <input
+          id="speedRange"
+          className="w-full"
+          type="range"
+          min={1000}
+          max={5000}
+          step={1000}
+          value={speed}
+          onChange={(e) => setSpeed(Number(e.target.value))}
+        />
+        <div className="flex justify-between w-full absolute top-4">
+          {marks.map((mark) => (
+            <span key={mark} className="text-gray-500 text-l m-2">
+              {mark}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SlideSpeed;
